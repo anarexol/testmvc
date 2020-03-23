01c8247b081fe85c54486295aa4568e34ae20ac3
Martin von der Burg <martin@vonderburg.de>
2020-03-23 10:36:44 +0100

    Add project files.

---
+++ testmvc/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using testmvc.Models;
+
+namespace testmvc.Controllers {
+    public class HomeController : Controller {
+
+        public ViewResult Index() {
+
+            return View("Splash");
+        }
+
+        [HttpGet]
+ 
+        [HttpPost]
+        public ActionResult Zskrechner(ZSKbag zsk) {
+
+            
+
+            ViewBag.Ziegen = zsk.Ziegen * 300;
+            ViewBag.Schafe = zsk.Schafe * 200;
+            ViewBag.Kuehe = zsk.Kuehe * 800;
+            ViewBag.EUR = ViewBag.Ziegen + ViewBag.Schafe + ViewBag.Kuehe;
+            if (ViewBag.EUR != 0 && ViewBag.EUR != null)
+            {
+                ViewBag.Ausgabe = "Sie erhalten EUR: ";
+                
+            }
+            return View("zskrechner");
+
+        }
+
+    }
+}
---
+++ testmvc/Models/ZSKbag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testmvc.Models {
+    public class ZSKbag {
+        public int Ziegen { get; set; }
+        public int Schafe { get; set; }
+        public int Kuehe { get; set; }
+        public int EUR { get; set; }
+        public static string Ausgabe { get; set; } = "Sie erhalten EUR: ";
+
+
+    }
+}
